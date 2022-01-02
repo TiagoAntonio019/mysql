7b733993b71fd5afdb85d0255acbb29bd3a742ec
Tiago Antonio <tmoises@navelinknet.com.br>
2022-01-02 15:29:20 -0300

    Code implemented

--- mysql.php
+++ mysql.php
@@ -1,2 +1,32 @@
 <?php
     #Code created by Tiago Antonio
+    class SQL{
+        private $Usuario = "root";
+        private $Senha = "";
+        private $Host = "localhost";
+        private $DB = "";
+        private $Conn;
+
+        public function __construct(){
+            #Open connection
+            $this->$Conn = mysqli_connect($this->Host, $this->Usuario, $this->Senha, $this->DB);
+            mysqli_set_charset($this->Conn, 'utf-8');
+        }
+
+        public function __destruct(){
+            #Close connection
+            mysqli_close($this->Conn);
+        }
+
+        public function Run($Command){
+            #INSERT, UPDATE, DELETE
+            mysqli_query($this->Conn, $Command);
+        }
+
+        public function Select($Command){
+            #SELECT
+            $Result = mysqli_query($this->Conn, $Command);
+            $Rows = musqli_fetch_all($Result);
+            return $Rows;
+        }
+    }
